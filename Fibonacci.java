3b4f2ac445359dd40fd55b7ac52298e932f6497d
Kalpesh-7781 <kalpesh.mewada1212@gmail.com>
2024-06-19 21:36:31 +0530

    first commit

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void fibonacciSeries(int n ){
+         int num1 = 0 ,num2 = 1;
+         for (int i = 0; i < n; i++){
+            System.out.print(num1 + " ");
+
+            int num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+         }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter N:");
+        int N = sc.nextInt();
+        fibonacciSeries(N);
+
+        sc.close();
+
+    }
+    
+}
