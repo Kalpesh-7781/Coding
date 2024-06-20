78039a6788f339d9c3e831238fb457b3266e8cd2
Kalpesh-7781 <kalpesh.mewada1212@gmail.com>
2024-06-20 20:11:49 +0530

    first commit

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class PrimeNumber {
 
-    static void primeCal(int num)
+    /*static void primeCal(int num)
    {
 	int count=0;
 	for(int i=1;i<=num;i++)
@@ -13,14 +13,28 @@ public class PrimeNumber {
 	   }
 	}
 	if(count==2)
-	       System.out.println("prime number ");
+	System.out.println("prime number ");
 	else
 	System.out.println("Not a prime number ");        
-   } 
+   } */
     public static void main(String[] args) {
-    System.out.println("Enter a number ");
-	Scanner sc=new Scanner(System.in);
-	int n=sc.nextInt();
-	primeCal(n);
-    }
+        int i,count;
+        System.out.print("Enter n value : ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("Prime numbers between 1 to "+n+" are ");
+        for(int j=2;j<=n;j++)
+        {
+            count=0;
+            for(i=1;i<=j;i++)
+            {
+            if(j%i==0)
+            {
+                count++;        
+            }
+        }
+        if(count==2)
+        System.out.print(j+"  ");     
+        }
+}
 }
