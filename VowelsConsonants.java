e7378cdb15160fcf6524e16f6c202ba727d8d139
Kalpesh-7781 <kalpesh.mewada1212@gmail.com>
2024-06-26 14:01:27 +0530

    first commit

---
+++ VowelsConsonants.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class VowelsConsonants {
+    public static void main(String[] args) {
+        int vow=0 , con=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word to count of  Vowels & Consonants: ");
+        String str = sc.next();
+        str = str.toLowerCase();
+
+        for(int i = 0; i < str.length(); i++){
+        char ch = str.charAt(i);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+        vow++;
+        }
+        else if (ch >= 'a' && ch <= 'z'){
+        con++;
+    }
+    }
+    System.out.println("Count of vowel:"+vow);
+    System.out.println("Count of Consonant:"+con);
+    }
+}
