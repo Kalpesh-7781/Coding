704c4bd6160e1159295370bc31040b52e45912b9
Kalpesh-7781 <kalpesh.mewada1212@gmail.com>
2024-07-03 19:12:44 +0530

    first commit

---
+++ SecondLargestElement.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SecondLargestElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size:");
+        int n  = sc.nextInt();
+        System.out.println("Array:");
+        int arr[]= new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int SecondLargestElement = secondLargeElement(arr);
+        System.out.println("SecondLargestElement:"+SecondLargestElement);
+
+    }
+
+    public static int secondLargeElement(int arr[]){
+        Arrays.sort(arr);
+
+        int max = arr[0];
+        int secondLarge =arr[0];
+        for(int i = 1; i<arr.length; i++){
+            if (arr[i]>max) {
+                max= arr[i];
+            }
+            max= arr[i-1];
+            secondLarge = max;
+
+        }
+        return secondLarge;
+    }
+
+
+    
+}
