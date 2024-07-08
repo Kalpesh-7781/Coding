0cd75a414bd727d3eeab1aaa275f4dfa0eba3c13
Kalpesh-7781 <kalpesh.mewada1212@gmail.com>
2024-07-08 13:37:26 +0530

    first commit

--- Armstrong.java
+++ Armstrong.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Armstrong {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no. : ");
+        int n = sc.nextInt();
         
     }
     
