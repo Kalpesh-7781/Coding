4659698cf3f5f870e5f26573dcb552d5e23f1eb1
Kalpesh-7781 <kalpesh.mewada1212@gmail.com>
2024-07-08 13:52:57 +0530

    first commit

--- Armstrong.java
+++ Armstrong.java
@@ -5,7 +5,22 @@ public class Armstrong {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the no. : ");
         int n = sc.nextInt();
-        
+
+        if(isArmstrong(n)){
+            System.out.println("Armstrong");
+        } else {
+            System.out.println("not Armstrong");
+        }
     }
-    
+
+    public static boolean isArmstrong(int n){
+        int originalNum = n;
+        int result = 0;
+        while (n!= 0) {
+            int digit = n%10;
+            result += (digit*digit*digit);
+            n /=10;
+        }
+        return result==originalNum;
+    }
 }
