d36180c65f4875bb311dc4f66d8fe0d3c0c18c7b
Kalpesh-7781 <kalpesh.mewada1212@gmail.com>
2024-08-08 15:04:07 +0530

    first commit

--- NumberOfDigits.java
+++ NumberOfDigits.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class NumberOfDigits {
+    
     public static void main(String[] args) {
+        int n=0, a=0;
+        System.out.print("Enter n  : ");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+
+        
+            while (n>0) {
+                n=n/10;
+                a++;
+
+            }
+            System.out.println("Digits:"+a);
+            
+        }
         
     }
     
-}
+
